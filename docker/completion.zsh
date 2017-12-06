2c2053cce9d96c12ec161185a8f8ffa572fcdb7e
Florian Mientjes <florian@mientjes.nl>
2017-12-06 10:38:37 +0100

    removed slow docker-machine completion

--- docker/completion.zsh
+++ docker/completion.zsh
@@ -1,14 +1,2 @@
 
-
-# compctl -K _dockermachines denv
-
-
 compctl -g '~/.docker/machine/machines/*(:t:r)' denv
-
-
-
-_dockermachines() {
-	completions="$(docker-machine ls --format "{{.Name}}")"
-
-	reply=("${(ps:\n:)completions}")
-}
