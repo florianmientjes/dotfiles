8b335c464cbc668973b2014341b1d4c0f59b4cd0
Florian Mientjes <florian@mientjes.nl>
2020-02-25 21:08:34 +0100

    feat: bb alias (bind npm bin)

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -19,6 +19,9 @@ alias ~="cd ~"
 
 alias sudo='sudo '
 
+
+alias bb='PATH=$(npm bin):$PATH;'
+
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias localip="ipconfig getifaddr en0"
 alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
